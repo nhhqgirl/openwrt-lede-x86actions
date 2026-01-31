164c8cd6391e271c8dfa928a5e18cd684bda6e77
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2026-01-31 22:38:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -50,10 +50,4 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/luc
 sed -i 's/services\/nlbw/nlbw/g; /path/s/admin\///g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
 sed -i 's/services\///g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
 
-# 设置samba4独立菜单
-sed -i 's/services\/samba4/samba4/g; /path/s/admin\///g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
-sed -i 's/services\///g' feeds/luci/applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4/config.js
 
-# 设置wol独立菜单
-sed -i 's/services\/wol/wol/g; /path/s/admin\///g' feeds/luci/applications/luci-app-wol/root/usr/share/luci/menu.d/luci-app-wol.json
-sed -i 's/services\///g' feeds/luci/applications/luci-app-wol/htdocs/luci-static/resources/view/wol/config.js
